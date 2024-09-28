8250a53f6a33fb4ef5ca14b90bb81d8e5bb522c6
Minh-Thong2211 <thelong.workhard@gmail.com>
2024-09-28 23:36:20 +0700

    fix after merge

--- FKitShop/src/App.jsx
+++ FKitShop/src/App.jsx
@@ -3,10 +3,8 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
-import SignInAndSignUp from './page/SignInAndSignUp'
-import ViewProfile from './page/ViewProfile/ViewProfile'
+import SignInSignUp from './component/SignInSignUp'
 import Header from './component/Header/Header'
-import Footer from './component/Footer/Footer'
 import HomePage from './component/HomePage/HomePage'
 import ViewUserProfile from './page/ViewUserProfile'
 
@@ -21,8 +19,8 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/home" element={<HomePage />} />
-      <Route path="/signin" element={<SignInAndSignUp />} />
-      <Route path="/signup" element={<SignInAndSignUp />} />
+      <Route path="/signin" element={<SignInSignUp />} />
+      <Route path="/signup" element={<SignInSignUp />} />
       <Route path="/profile" element={<ViewUserProfile />} />
     </Routes>
     
