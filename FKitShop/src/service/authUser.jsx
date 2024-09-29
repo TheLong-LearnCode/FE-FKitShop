5c51ef47106628af9640f9cd2fe83e75a8b5beae
To Minh Tuyen <tominhtuyen18092k4@gmail.com>
2024-09-30 00:26:12 +0700

    axiosv2

--- FKitShop/src/service/authUser.jsx
+++ FKitShop/src/service/authUser.jsx
@@ -1,25 +1,25 @@
 import api from "../config/axios.jsx";
 
-export const loginUser = async (data) => {
+export const loginUser = async (data, navigate) => {
   try {
     const response = await api.post("/auth/login", data); // data should already be a JSON object
+    navigate("/");
     return response.data;
   } catch (error) {
     throw error;
   }
 };
 
-export const signUpUser = async (data) => {
+export const signUpUser = async (data, navigate) => {
   try {
     const response = await api.post("/accounts", JSON.stringify(data), {
       headers: {
         'Content-Type': 'application/json',
       },
     });
+    navigate("/signin");
     return response.data;
   } catch (error) {
     throw error;
   }
 };
-
-
