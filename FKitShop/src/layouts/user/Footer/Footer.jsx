c0d38ed483a8bb33967df7ef97546f8c4bcdac0f
Minh-Thong2211 <thelong.workhard@gmail.com>
2024-10-03 12:33:37 +0700

    update fix

--- FKitShop/src/layouts/user/Footer/Footer.jsx
+++ FKitShop/src/layouts/user/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Footer.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'boxicons'
 
 export default function Footer() {
--- FKitShop/src/page/user/home/HomePage.jsx
+++ FKitShop/src/page/user/home/HomePage.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import './HomePage.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import CardMainContent from '../product/card/CardMainContent';
 import { useSelector } from 'react-redux';
 
