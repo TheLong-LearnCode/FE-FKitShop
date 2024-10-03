2f3ca97072885467d0aef561a2c313f747f8a24e
To Minh Tuyen <tominhtuyen18092k4@gmail.com>
2024-10-03 13:38:37 +0700

    implement ProtectedRoute

---
+++ FKitShop/src/hooks/useAuthen.jsx
@@ -0,0 +1,32 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { loadUserFromCookie } from '../service/authUser';
+
+const useAuthen = () => {
+  const dispatch = useDispatch();
+  const { data: userData, status } = useSelector((state) => state.auth);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // If there's no user data in the state, try loading from cookie
+    if (!userData) {
+      dispatch(loadUserFromCookie());
+    } else {
+      setLoading(false);
+    }
+  }, [dispatch, userData]);
+
+  useEffect(() => {
+    if (status === 'IDLE' || status === 'PENDING') {
+      setLoading(true);
+    } else {
+      setLoading(false);
+    }
+  }, [status]);
+
+  const userRole = userData ? userData.role : null; // Assuming `userData` contains a `role` field
+
+  return { userRole, loading };
+};
+
+export default useAuthen;
